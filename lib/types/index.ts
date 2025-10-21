81c21d84d51b1978ef1562158bfca18d4d1fe426
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-10-21 16:33:07 +0200

    chore: added `OptionalKeys<T>` type to `#lib`
    This type extracts only optional keys from a type. This can be combined with
    the `RequireAll<T>` type to require all optional properties.

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,5 +1,6 @@
 /* istanbul ignore file */
 
 export * from "#lib/types/require-all";
+export * from "#lib/types/optional-keys";
 export * from "#lib/types/log-level";
 export * from "#lib/types/tag";
