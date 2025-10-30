e12b9aea69f53ac4d15ba4d4350c70ceaab523cc
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-10-30 15:22:55 +0100

    chore(deps): added `yargs` as new CLI parser
    There were some issues with `Commander`, `yargs` is supposed to fix them.

--- src/cli.ts
+++ src/cli.ts
@@ -1,111 +1,188 @@
 #!/usr/bin/env node
 
-import { BasePath, ExitCode, Host, LogLevel, Logger } from '#lib';
-import { Command } from 'commander';
-import { CompileArgs } from '#src/compile';
+import * as zod from 'zod';
+import { BasePath, type DeepReadonly, Host, LogLevel, Logger } from '#lib';
+import { CompileArgs, compile } from '#src/compile/index';
+import FastGlob from 'fast-glob';
 import { ServeArgs } from '#src/serve';
+import chalk from 'chalk';
+import { hideBin } from 'yargs/helpers';
 import path from 'path';
 import pkg from '#package' with { type: 'json' };
+import yargs from 'yargs';
 
-// Declare CLI options
+// Helpers
 
-/** The root CLI argument parser. */
-export const program = new Command();
+const NAME = pkg.name
+const VERSION = pkg.version;
+const EPILOGUE = `
+For the documentation go to ${chalk.underline(pkg.homepage)}. You can report bugs or create request features by opening an 
+issue on ${chalk.underline(pkg.bugs.url)}, or even better yet a pull request. To see the source code for yourself, you can
+go to ${chalk.underline(pkg.repository.url)}.
+`
+  .split('\n')
+  .map((line) => line.trim())
+  .filter(Boolean)
+  .join(' ');
 
-program
-  .name(pkg.name)
-  .allowUnknownOption(false)
-  .allowExcessArguments(false)
-  .exitOverride()
-  .combineFlagAndOptionalValue(false)
-  .version(pkg.version, '-v, --version', 'print program version and then exit')
-  .option(
-    '-i, --include-import <glob>',
-    'a directory or file to import and use as custom tags',
-    // eslint-disable-next-line no-sequences, @typescript-eslint/prefer-readonly-parameter-types
-    (value, previous) => (previous.push(value), previous),
-    [] as string[]
-  )
-  .option(
-    '-I, --ignore-in-include-import <glob>',
-    'a directory or file to not import and use as custom tags',
-    // eslint-disable-next-line no-sequences, @typescript-eslint/prefer-readonly-parameter-types
-    (value, previous) => (previous.push(value), previous),
-    [] as string[]
-  )
-  .option(
-    '-l, --log-level <level>',
-    `To log level to use when running. Must be one of: ${LogLevel.options.join(', ')}`,
-    (value) => LogLevel.parser.parse(value),
-    Logger.DEFAULT_LOG_LEVEL
-  )
-  .option(
-    '-w, --watch <level>',
-    `watch the specified files, and rerun if they change.`,
-    (value) => Boolean(value),
-    false
-  )
-  .option(
-    '-V, --verbose',
-    'wether to be more talkative. Raises log level by one per flag.',
-    // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-    (_ignore, previous) => previous + 1,
-    LogLevel.toNumber(LogLevel.SILENT)
-  );
-
-const compileFunctionWrapper = function compileFunctionWrapper(...args: readonly unknown[]): void {
-  Logger.critical(args);
-  /* Temp: await mergeArgsAndExec(command, compileFile); */
+/** If argument is a string, return it, if it is an array of strings, return the last one. */
+const getStringOrLast = function getStringOrLast(value: string | readonly string[]): string {
+  if (typeof value === 'string') return value;
+  return value[value.length - 1];
 };
 
-/** The CLI argument parser for the `compile` sub-command. */
-export const compile = program
-  .command('compile [file]')
-  .description('compile and then write the changes')
-  .action(compileFunctionWrapper)
-  .option(
-    '-o, --output <path>',
-    'The path to write the output to',
-    (value) => path.resolve(value),
-    CompileArgs.defaults.output
-  );
+// CLI arguments
 
-const serveFunctionWrapper = function serveFunctionWrapper(...args: readonly unknown[]): void {
-  Logger.critical(args);
-  /* Temp: await mergeArgsAndExec(command, compileFile); */
-};
+/** 
+ * The root CLI argument parser.
+ */
+export const cli = yargs(hideBin(process.argv))
+  .strict()
+  .wrap(null)
+  .demandCommand(1, 'You need to specify a subcommand')
+  .scriptName(NAME)
+  .version("version", "Show the program version, and then exit.", VERSION)
+  .alias('v', 'version')
+  .help("help", "Show this help message, and then exit.", true)
+  .alias('h', 'help')
+  .epilogue(EPILOGUE)
 
-/** The CLI argument parser for the `serve` sub-command. */
-export const serve = program
-  .command('serve [file]')
-  .description('compile and then serve on a webserver')
-  .action(serveFunctionWrapper)
-  .option(
-    '-H, --host <ip-address>',
-    'The host to use ports from.',
-    (value) => Host.parse(value),
-    ServeArgs.defaults.host
+  .option('plugins', {
+    alias: 'p',
+    array: true,
+    coerce: (value: readonly string[]) => FastGlob.sync([...value]),
+    default: [] as string[],
+    description: 'A directory or file to import and use as custom tags',
+    type: 'string',
+  })
+
+  .option('log-level', {
+    alias: 'l',
+    choices: LogLevel.options,
+    coerce: (value: string | readonly string[]) => LogLevel.parser.parse(getStringOrLast(value)),
+    default: Logger.DEFAULT_LOG_LEVEL,
+    description: `Log level to use when running.`,
+    type: 'string',
+  })
+
+  .option('watch', {
+    alias: 'w',
+    default: false,
+    description: 'Watch the specified files, and rerun if they change.',
+    type: 'boolean',
+  })
+
+  .option('verbose', {
+    alias: 'V',
+    default: 0,
+    description: 'Whether to be more talkative. Makes it more verbose by one step one per flag.',
+    type: 'count',
+  })
+
+  // Middlewares
+
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types, prefer-arrow-callback
+  .middleware(function setLoglevelOnLogger(argv): void {
+    // More verbose for the value of the count of `verbose`:
+    const verbosity = LogLevel.toNumber(argv.logLevel) - argv.verbose;
+    Logger.logLevel = LogLevel.fromNumber(verbosity);
+    Logger.debug(`Set LogLevel to: ${Logger.logLevel}`);
+  })
+
+  // Compile subcommand
+  .command(
+    'compile [file]',
+    'Compile and then write the results to disk.',
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types,
+    (subYargs) =>
+      subYargs.epilogue(EPILOGUE)
+
+        .option('output', {
+          alias: 'o',
+          coerce: (value: string | readonly string[]) => path.resolve(getStringOrLast(value)),
+          default: CompileArgs.defaults.output,
+          description: 'The path to write the output to',
+          type: 'string',
+        })
+
+        .positional('file', {
+          alias: 'input',
+          default: CompileArgs.defaults.file,
+          describe: 'The file to compile, and then serve',
+          type: 'string',
+        }),
+
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+    async (args) => {
+      await compile(args as DeepReadonly<CompileArgs>);
+    }
   )
-  .option(
-    '-p, --port <int>',
-    'The port to listen on and serve the slides from.',
-    (value) => Number(value),
-    ServeArgs.defaults.port
+
+  // Serve subcommand
+  .command(
+    'serve [file]',
+    'Compile and then serve the results on a webserver.',
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types,
+    (subYargs) =>
+      subYargs.epilogue(EPILOGUE)
+
+        .option('host', {
+          alias: 'H',
+          coerce: (value: string | readonly string[]) => Host.parser.parse(getStringOrLast(value)),
+          default: ServeArgs.defaults.host,
+          description: 'The host to use ports from.',
+          type: 'string',
+        })
+
+        .option('port', {
+          alias: 'P',
+          coerce: (value: string | number) => zod.number().parse(value),
+          default: ServeArgs.defaults.port,
+          description: 'The port to listen on and serve the slides from.',
+          type: 'number',
+        })
+
+        .option('base-path', {
+          alias: 'B',
+          coerce: (value: string) => BasePath.parser.parse(value, {
+            error: () => `option '--base-path' must be a valid path starting with /`,
+          }),
+          default: ServeArgs.defaults.basePath,
+          description: 'The base path to serve from. If specified, serve from that subdirectory.',
+          type: 'string',
+        })
+
+        .positional('file', {
+          alias: 'input',
+          default: CompileArgs.defaults.file,
+          describe: 'The file to compile, and then serve',
+          type: 'string',
+        }),
+
+    // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+    (args) => {
+      Logger.critical('serve', args);
+      // Temp: await mergeArgsAndExec(command, serveFile);
+    }
   )
-  .option(
-    '-b, --base-path. <prefix>',
-    'The base path the serve from. If specified serve from that subdirectory.',
-    (value) => BasePath.parse(value),
-    ServeArgs.defaults.basePath
-  );
+
+  // Argument fails
+
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types,, no-restricted-syntax
+  .fail((message?: string, error?: Error) => {
+
+    if (error) {
+      Logger.critical(error.message);
+      process.exit(1);
+    }
+
+    Logger.critical('Invalid usage:', message);
+    process.exit(1);
+  });
+
+
+// If file is being executed instead of sourced:
 
 const fileIndex = 1;
 const isBeingExecuted = import.meta.url === `file://${process.argv[fileIndex]}`;
-if (isBeingExecuted) {
-  try {
-    program.parse(process.argv, { from: 'node' });
-  } catch (error: unknown) {
-    const EXIT_CODE = ExitCode.from(error);
-    process.exit(EXIT_CODE);
-  }
-}
+if (isBeingExecuted) await cli.parse();
