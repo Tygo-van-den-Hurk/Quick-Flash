7422ca451fde763f67a46f12751b12e20a4302e4
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-10-30 15:22:55 +0100

    chore(deps): added `yargs` as new CLI parser
    There were some issues with `Commander`, `yargs` is supposed to fix them.

--- test/src/cli.test.ts
+++ test/src/cli.test.ts
@@ -1,29 +1,10 @@
-import { compile, program, serve } from '#src/cli';
 import { describe, expect, test } from 'vitest';
-import { Command } from 'commander';
+import { cli } from '#src/cli';
 
 describe('program', () => {
   // Tests relating `program`:
 
   test('is defined', () => {
-    expect(program).toBeInstanceOf(Command);
-  });
-});
-
-describe('compile', () => {
-  // Tests relating `compile`:
-
-  test('to be subcommand of program', () => {
-    expect(compile).toBeInstanceOf(Command);
-    expect(compile.parent).toBe(program);
-  });
-});
-
-describe('serve', () => {
-  // Tests relating `serve`:
-
-  test('to be subcommand of program', () => {
-    expect(serve).toBeInstanceOf(Command);
-    expect(serve.parent).toBe(program);
+    expect(cli).toBeDefined();
   });
 });
