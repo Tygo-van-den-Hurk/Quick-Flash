f31233b91d7eb1a7e0a6a3de6f8c2e2a5665730f
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-10-31 12:16:58 +0100

    style(format): `npm run format`
    I should really make a pre-commit hook for this.

--- src/compile/parse.ts
+++ src/compile/parse.ts
@@ -17,22 +17,23 @@ export const parseInputContent = function parseInputContent(
     isSilent: LogLevel.of(Logger.logLevel).isLessVerboseThen(LogLevel.INFO),
     prefixText: chalk.cyan(LogLevel.INFO.toUpperCase()),
   }).start(`Attempting to parse input...`);
-  
+
   try {
     const desired: XmlParserNodeType[] = ['Element', 'Text'] as const;
     const filter = (node: Readonly<XmlParserNode>): boolean => desired.includes(node.type);
     const strictMode = true;
     const result = parseXML(content, { filter, strictMode });
-    spinner.succeed(`${chalk.green("Success")}: parsed input!`);
+    spinner.succeed(`${chalk.green('Success')}: parsed input!`);
     return Result.ok(result);
   } catch (error) {
-
     if (error instanceof Error) {
       spinner.fail(`${chalk.red(LogLevel.ERROR.toUpperCase())}: ${error.message}`);
       return Result.error(error);
     }
 
-    spinner.fail(`${chalk.red(LogLevel.ERROR.toUpperCase())}: an error occurred while parsing the input.`);
+    spinner.fail(
+      `${chalk.red(LogLevel.ERROR.toUpperCase())}: an error occurred while parsing the input.`
+    );
     return Result.error(new Error(`Could not parse input file.`));
   }
 };
