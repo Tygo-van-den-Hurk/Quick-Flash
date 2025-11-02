4ac30f305393a287f506aa1caefbc5a594f28b6c
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-11-03 00:36:25 +0100

    chore: changed the import path to the exact file
    Since we changed the export name of the markup renderers by prefixing them.

--- test/lib/core/markup/markdown.test.ts
+++ test/lib/core/markup/markdown.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { MarkdownRenderer } from '#lib';
+import { MarkdownRenderer } from '#lib/core/markup/markdown';
 
 describe('class MarkdownRenderer implements MarkupRender', () => {
   test(`rendering an empty string to see if it stays a literal`, () => {
