1568477b9da06c4d58c63c1da0315e063159d92c
Tygo van den Hurk <91738110+Tygo-van-den-Hurk@users.noreply.github.com>
2025-11-04 20:11:50 +0100

    test: test for if the authors show up on a title slide
    This improves coverage to 100% for `TitleSlide`.

--- test/lib/core/components/presentations/presentation.test.ts
+++ test/lib/core/components/presentations/presentation.test.ts
@@ -1,38 +1,54 @@
 import { describe, expect, test } from 'vitest';
 import { Component } from '#lib/core/components/class';
-import { Presentation } from '#lib/core/components/presentations/presentation';
+import { TitleSlide } from '#lib/core/components/slides/title-slide';
 
-describe('class Presentation implements Component', () => {
-  test('Registered', () => {
-    expect(Component.retrieve(Presentation.name)).toBe(Presentation);
+describe('class TitleSlide implements Component', () => {
+  test('Is registered as a component', () => {
+    expect(Component.retrieve(TitleSlide.name)).toBe(TitleSlide);
   });
 
   const construct = {
-    attributes: {
-      title: 'BEST SOFTWARE EVER',
-    },
+    attributes: {},
     focusMode: 'default' as const,
     id: '',
-    level: 0,
+    level: 1,
     path: ['root'],
   };
 
   test('Is creatable', () => {
-    expect(() => new Presentation({ ...construct })).not.toThrow();
+    expect(() => new TitleSlide({ ...construct })).not.toThrow();
   });
 
   const render = {
     children: (): string => '',
   };
 
-  test('renders with children', () => {
-    expect(() => new Presentation({ ...construct }).render({ ...render })).not.toThrow();
+  test('renders author if present', () => {
+    const author = 'Tygo van den Hurk';
+    const attributes = { author };
+    const slide = new TitleSlide({ ...construct, attributes });
+    expect(slide.render({})).toContain(author);
   });
 
-  test('does not render without children', () => {
+  test('renders all authors if present', () => {
+    const author1 = 'Tygo van den Hurk';
+    const author2 = 'John Doe';
+    const authors = `${author1}, ${author2}`;
+    const attributes = { authors };
+    const slide = new TitleSlide({ ...construct, attributes });
+    const result = slide.render({});
+    expect(result).toContain(author1);
+    expect(result).toContain(author2);
+  });
+
+  test('renders with children', () => {
+    expect(() => new TitleSlide({ ...construct }).render({ ...render })).not.toThrow();
+  });
+
+  test('renders without children', () => {
     expect(() =>
       // eslint-disable-next-line no-undefined
-      new Presentation({ ...construct }).render({ ...render, children: undefined })
-    ).toThrow();
+      new TitleSlide({ ...construct }).render({ ...render, children: undefined })
+    ).not.toThrow();
   });
 });
